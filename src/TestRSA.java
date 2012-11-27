3a17dfc9a11df962d3d8f7523afe95705f64b30d
Jeff <zip.github@goldsy.net>
2012-11-26 22:31:32 -0500

    A couple comments and some formatting.

--- src/TestRSA.java
+++ src/TestRSA.java
@@ -6,20 +6,25 @@ public class TestRSA {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// This is a general idea on how to get the bits from a string.
 		String s = "foo";
-		  byte[] bytes = s.getBytes();
-		  StringBuilder binary = new StringBuilder();
-		  for (byte b : bytes)
-		  {
-		     int val = b;
-		     for (int i = 0; i < 8; i++)
-		     {
-		        binary.append((val & 128) == 0 ? 0 : 1);
-		        val <<= 1;
-		     }
-		     binary.append(' ');
-		  }
-		  System.out.println("'" + s + "' to binary: " + binary);
+		byte[] bytes = s.getBytes();
+		StringBuilder binary = new StringBuilder();
+		
+		for (byte b : bytes)
+		{
+			int val = b;
+			
+			for (int i = 0; i < 8; i++)
+			{
+				binary.append((val & 128) == 0 ? 0 : 1);
+				val <<= 1;
+			}
+			
+			binary.append(' ');
+		}
+		
+		System.out.println("'" + s + "' to binary: " + binary);
 	}
 
 }
